277145353bcce86779deabcdf5a1f6db6f9e50af
Obafemi Fadairo <ofpupp@princeton.edu>
2018-11-13 00:13:58 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   get 'home', to: 'static#home'
   resources :orders
   resources :invoices
-  resources :products, only: [:create, :inventory]
+  resources :products, only: [:create, :inventory, :description]
 
 end
