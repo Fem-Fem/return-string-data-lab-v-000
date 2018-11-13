a29e6cdcd4cd6393357bbe23c4b664b2e3fbc9cf
Obafemi Fadairo <ofpupp@princeton.edu>
2018-11-13 00:13:02 +0000

    automatically backed up by learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,31 @@
 class ProductsController < ApplicationController
 
+  def create
+    # binding.pry
+    @product = Product.create(product_params)
+    redirect_to products_path
+  end
+
+  def inventory
+    @product = Product.find(params[:id])
+    if @product.inventory == nil
+      reutrn false
+    else
+      return true
+    end
+    binding.pry
+  end
+
+  def products
+    @products = Product.all
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :price, :inventory, :description)
+  end
+
+
+
 end
