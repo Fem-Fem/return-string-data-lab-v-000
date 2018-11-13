a8a5fe9ee8e4646847f7d592f2f2ff58a040a012
Obafemi Fadairo <ofpupp@princeton.edu>
2018-11-13 00:17:00 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   resources :invoices
   resources :products, only: [:create]
   get '/posts/:id/body', to: 'posts#body'
-  get /products/:id/inventory
-  get /products/:id/description
+  get '/products/:id/inventory', to 'products#inventory'
+  get '/products/:id/description', to 'products#description'
 end
